ccee43547230de94b784cdb44b150fe18998eaa9
md-abu-kayser <abu.kayser.official@gmail.com>
2026-08-28 02:51:01 +0600

    build(vite): add @ path alias to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  define: {
-    "process.env.API_KEY": JSON.stringify("env.GEMINI_API_KEY"),
-    "process.env.GEMINI_API_KEY": JSON.stringify("env.GEMINI_API_KEY"),
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
